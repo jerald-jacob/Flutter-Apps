e02aec0e0a31726d90a373eb06d11c33984755ab
jerald-jacob <jeraldjacob10@gmail.com>
2019-12-19 11:00:20 +0530

    code alignment corrected

--- Hangman/lib/screens/words/get_words_list.dart
+++ Hangman/lib/screens/words/get_words_list.dart
@@ -4,68 +4,58 @@ import 'package:hangman/dao/hm_words.dart';
 
 import 'get_words_list_presenter.dart';
 
-
 class GetWordsList extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return GetWordsListPresenterState();
-
   }
 }
 
-class GetWordsListPresenterState extends State<GetWordsList> implements GetWordsListPresenterContract {
+class GetWordsListPresenterState extends State<GetWordsList>
+    implements GetWordsListPresenterContract {
   GetWordsListPresenter _presenter;
-  List<HMWords>_wordsList=new List();
-  GetWordsListPresenterState(){
-    _presenter=new GetWordsListPresenter(this);
+  List<HMWords> _wordsList = new List();
+
+  GetWordsListPresenterState() {
+    _presenter = new GetWordsListPresenter(this);
   }
 
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
-body: ListView(
-  children: <Widget>[
-    Text("hai")
-  ],
-),
+      body: ListView(
+        children: <Widget>[Text("hai")],
+      ),
     );
   }
+
   @override
   void dispose() {
     super.dispose();
   }
+
   @override
   void initState() {
     super.initState();
     _wordsAPICall(true);
   }
 
-  void _wordsAPICall(bool clear) async
-  {
- //   _presenter = new GetWordsListPresenter(this);
+  void _wordsAPICall(bool clear) async {
+    //   _presenter = new GetWordsListPresenter(this);
     _presenter.doSyncWordList();
   }
 
   @override
   RestDataSource api;
 
-
-
   @override
-  void onGroupListSuccess(data) {
-
-  }
+  void onGroupListSuccess(data) {}
 
   @override
   void onWordsSyncComplete(HMWordsList data) {
-
     _wordsList.addAll(data.diaryList);
     print("data fetch success");
-
   }
-
-
 }
 
 abstract class ItemClickCallback {
